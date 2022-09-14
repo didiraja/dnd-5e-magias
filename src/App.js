56c1837ac95d319e255b05a7c8a3b2568bb6144e
didiraja <didiraja@hotmail.com>
2022-09-14 01:33:06 -0300

    refactor: 💡 WIP: started fetching with useReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,103 @@
-import logo from './logo.svg';
-import './App.scss';
+import React, { useState, useEffect, useReducer, } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import axios from "axios";
 
-function App() {
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return { ...state, isLoading: true, isError: false };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const useDataApi = (initialUrl, initialData) => {
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+
+      try {
+        const result = await axios(url);
+
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: 'FETCH_FAILURE' });
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+
+  return [state, setUrl];
+};
+
+
+const App = () => {
+
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+    { hits: [] },
+  );
+
+  // const data = {
+  //   dataSource: "personal-app",
+  //   database: "app",
+  //   collection: "spell-list",
+  //   filter: {}
+  // };
+
+  // const config = {
+  //   method: "post",
+  //   url: "https://us-east-1.aws.data.mongodb-api.com/app/data-njmii/endpoint/data/v1/action/find",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     // "Access-Control-Request-Headers": "*",
+  //     "api-key":
+  //     "xaab4t4xmxJrAaHSPaoOJiz4rIPqFnW3XKKwhwb4nksPEAQwvPVQuTaZreSbEvfi",
+  //   },
+  //   data: data,
+  // };
+
+	// axios(config)
+	// 	.then((response) => {
+	// 		useSpells(response.data.documents)
+	// 	})
+	// 	.catch((error) => {
+	// 		console.log(error);
+	// 	});
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +114,14 @@ function App() {
           Learn React
         </a>
       </header>
+
+			{/* {
+				spells.map((spell, index) => {
+
+					
+
+				})
+			} */}
     </div>
   );
 }
