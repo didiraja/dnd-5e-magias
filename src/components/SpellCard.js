058df7c826b624e82163c57543b4f064e9f7b4d9
didiraja <didiraja@hotmail.com>
2022-09-20 12:33:08 -0300

    style: 💄 starting style on cards

--- src/components/SpellCard.js
+++ src/components/SpellCard.js
@@ -1,94 +1,87 @@
 import React from "react";
+import './SpellCard.scss';
 
 const convertBool = (int) => {
 
-    const bool = Boolean(int);
+	const bool = Boolean(int);
 
-    if (bool)
-        return `${int}: ${bool}`
+	if (!bool)
+		return `disabled`
+
+	return;
 };
 
 const SpellCard = ({
-    SpellName, Classes, Level, School, Ritual, Concentration, Verbal,
-    Somatic, Time, Reach, Material, Duration, Description
+	SpellName, Classes, Level, School, Ritual, Concentration, Verbal,
+	Somatic, Time, Reach, Material, Duration, Description
 }) => {
 
 
-    return (
-    <div className="spell-card border border-slate-500 rounded-md mb-5 p-5">
+	return (
+		<div className="spell-card">
 
-        <div className="name font-bold text-3xl">
-            {SpellName}
-        </div>
+			<div className="Header">
+					<p>{SpellName}</p>
+					<p>{`${Level}º Nível`}</p>
+					<p>{School}</p>
+			</div>
 
-        <div className="props border-dotted border-y my-5 py-2 text-sm">
+			<div className="Description">
 
-            <div className="prop-item class">
-                {Classes}
-            </div>
+				<div className="props">
 
-            <div className="prop-item level">
-                {Level}º Círculo
-            </div>
+					<div className="classes">
+						{Classes}
+					</div>
 
-            <div className="prop-item School">
-                {School}
-            </div>
+					<div className="settings">
 
-            <div className="components">
-                {
-                    convertBool(Ritual) && 
+						<div className="button">
+							Time <span>{Time}</span>
+						</div>
+						<div className="button">
+							Alcance <span>{Reach}</span>
+						</div>
+						<div className="button">
+							Duração <span>{Duration}</span>
+						</div>
+					</div>
 
-                    <div className="prop-item">
-                        Ritual ✅
-                    </div>
-                }
-                {
-                    convertBool(Concentration) &&
-                    <div className="prop-item">
-                        Concentration ✅
-                    </div>
-                }
-                {
-                    convertBool(Verbal) &&
-                    <div className="prop-item">
-                        Verbal ✅
-                    </div>
-                }
-                {
-                    convertBool(Somatic) &&
-                    <div className="prop-item">
-                        Somatic ✅
-                    </div>
-                }
-                {
-                    convertBool(Material) &&
-                    <div className="prop-item">
-                        Material ✅
-                    </div>
-                }
-            </div>
+					<div className="type">
 
-            <div className="prop-item Time">
-                Time: {Time}
-            </div>
+						<div className={`button ${convertBool(Ritual)}`}>
+							Ritual
+						</div>
 
-            <div className="prop-item Reach">
-                Alcance: {Reach}
-            </div>
+						<div className={`button ${convertBool(Concentration)}`}>
+							Concentração
+						</div>
 
-            
-            <div className="prop-item Duration">
-                Duração: {Duration}
-            </div>
-        </div>
+						<div className={`button ${convertBool(Verbal)}`}>
+							Verbal
+						</div>
+
+						<div className={`button ${convertBool(Somatic)}`}>
+							Somático
+						</div>
+
+						<div className={`button ${convertBool(Material)}`}>
+							Material
+						</div>
+					</div>
+				</div>
+
+				<p>
+					{Description}
+				</p>
+			</div>
 
 
-        <div className="Description">
-            {Description}
-        </div> 
-    </div>
-    )
+			
+
+			
+		</div>
+	)
 }
 
 export default SpellCard;
